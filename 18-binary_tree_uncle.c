4b37630598eea82a8dbfcc3c98f1f2bd990b86a3
nachoneca <8666@holbertonstudents.com>
2024-04-04 20:59:40 +0000

    Add 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_uncle - find if have uncle
+ *@node: pointer to the node to find if have sibling
+ *Return: return pointer to uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent->parent == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+	{
+		if (node->parent->parent->right == NULL)
+			return (NULL);
+		return (node->parent->parent->right);
+	}
+	else
+	{
+		if (node->parent->parent->left == NULL)
+			return (NULL);
+		return (node->parent->parent->left);
+	}
+}
