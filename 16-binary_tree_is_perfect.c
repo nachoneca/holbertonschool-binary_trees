bc7ef87b8a1a66f2578a3f9e48807880f63dd849
nachoneca <8666@holbertonstudents.com>
2024-04-04 20:49:00 +0000

    Add 16-binary_tree_is_perfect.c

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,41 @@
+#include"binary_trees.h"
+int b_height(const binary_tree_t *tree)
+{
+        size_t count_l = 0, count_r = 0;
+
+        if (tree == NULL)
+                return (-1);
+
+        if (tree->left)
+                count_l = 1 + b_height(tree->left);
+        if (tree->right)
+                count_r = 1 + b_height(tree->right);
+        return (count_l > count_r ? count_l : count_r);
+}
+
+/**
+ *binary_tree_balance - calculates the balance in a func
+ *@tree: pointer to the root of the tree
+ *Return: returns the balance of the tree, tree->left - tree->right
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+        if (tree == NULL)
+                return (0);
+        return (b_height(tree->left) - b_height(tree->right));
+}
+/**
+ *
+ *
+ *
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_balance(tree->left) == 0 && binary_tree_balance(tree->right) == 0)
+		return (1);
+	else
+		return (0);
+}
