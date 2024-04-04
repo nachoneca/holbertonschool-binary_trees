77fcbf0d13f8205f02909eb0660a8797438e89c7
nachoneca <8666@holbertonstudents.com>
2024-04-04 19:13:13 +0000

    fix 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,6 @@
 #include"binary_trees.h"
-/**
- *binary_tree_balance - calculates the balance in a func
- *@tree: pointer to the root of the tree
- *Return: returns the balance of the tree, tree->left - tree->right
- */
-int binary_tree_balance(const binary_tree_t *tree)
+
+int b_height(const binary_tree_t *tree)
 {
 	size_t count_l = 0, count_r = 0;
 
@@ -15,5 +11,18 @@ int binary_tree_balance(const binary_tree_t *tree)
 		count_l = 1 + binary_tree_balance(tree->left);
 	if (tree->right)
 		count_r = 1 + binary_tree_balance(tree->right);
-	return (count_l - count_r);
+	return (count_l > count_r ? count_l : count_r);
+}
+
+/**
+ *binary_tree_balance - calculates the balance in a func
+ *@tree: pointer to the root of the tree
+ *Return: returns the balance of the tree, tree->left - tree->right
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == ((void *)0))
+		return (0);
+
+	return (b_height(tree->left) - b_height(tree->right));
 }
