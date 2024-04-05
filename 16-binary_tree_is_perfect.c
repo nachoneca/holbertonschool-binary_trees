ce91c7d0eaf109555e2ceb24ed2c50f5d2cfd0bf
root <alegresah@gmail.com>
2024-04-05 16:17:26 +0000

    Updated 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,5 +47,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (!binary_tree_is_perfect(tree->left) || !binary_tree_is_perfect(tree->right))
 		return (0);
 
-		return (1);
+	return (1);
 }
